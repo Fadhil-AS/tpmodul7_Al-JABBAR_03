96d6b94ec9a874f7bff8c8908a5c680ff28aa256
Bimo Zachriansyah Wicaksono Hermawan <bimozachri@gmail.com>
2023-04-02 16:32:41 +0700

    menambahkan class DataMahasiswa

--- tpmodul7_Al-JABBAR_03/Program.cs
+++ tpmodul7_Al-JABBAR_03/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul7_Al_JABBAR_03;
+
+DataMahasiswa_1302213012 mhs = new DataMahasiswa_1302213012();
+
+mhs.ReadJSON();
