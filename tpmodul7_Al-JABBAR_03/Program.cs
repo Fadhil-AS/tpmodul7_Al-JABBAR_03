6a85b271ce8d8332939bb01050e10bde261ec0b6
vikhanmuharram <vikhanmuharram@student.telkomuniversity.ac.id>
2023-04-02 16:21:00 +0700

    menambahkan class DataMahasiswa1302213089

--- tpmodul7_Al-JABBAR_03/Program.cs
+++ tpmodul7_Al-JABBAR_03/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using coba_coba;
+
+DataMahasiswa1302213089 mhs = new DataMahasiswa1302213089();
+
+mhs.ReadJSON();
