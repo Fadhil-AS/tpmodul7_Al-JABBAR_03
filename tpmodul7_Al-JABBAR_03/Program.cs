698512a20fc54727b7249b5ebdc5227b6c960983
Zeraume <farissiddiqrp@gmail.com>
2023-04-02 16:45:02 +0700

    menambahkan class DataMahasiswa1302213133

--- tpmodul7_Al-JABBAR_03/Program.cs
+++ tpmodul7_Al-JABBAR_03/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul7_Al_JABBAR_03;
+
+DataMahasiswa1302213133 mhs = new DataMahasiswa1302213133();
+mhs.readJSON();
