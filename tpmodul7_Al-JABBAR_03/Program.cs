e34dee9d23cf4dafdd7a05410aaf25d9e3182fe1
heruiksan <heruiksan43@gmail.com>
2023-04-02 18:41:10 +0700

    menambahkan class <KuliahMahasiswa1302210009>

--- tpmodul7_Al-JABBAR_03/Program.cs
+++ tpmodul7_Al-JABBAR_03/Program.cs
@@ -5,5 +5,7 @@ DataMahasiswa1302210009 mhsheru = new DataMahasiswa1302210009();
 
 mhsheru.ReadJSON();
 
+KuliahMahasiswa1302210009 kulheru = new KuliahMahasiswa1302210009();
 
+kulheru.ReadJSON();
 
